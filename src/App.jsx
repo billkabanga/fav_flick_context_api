b0e783f79c06311bc7a899bab8753a1baa2c595b
billkabanga <tkbillkabanga@gmail.com>
2019-08-05 13:02:25 +0300

    integrate tvmaze api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { Store } from './Store';
 
 const App = () => {
-  const store = useContext(Store);
+  const { state, dispatch } = useContext(Store);
+
+  const fetchDataAction = async () => {
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    });
+  };
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  })
+
   return (
     <Fragment>
-      {console.log('store----', store)}
+      {console.log('store----', state)}
       <div>
         <h1>Favourite Flick Site</h1>
         <p>Pick your favourite flicks/series</p>
+        <div>
+          {
+            state.episodes.map(episode => {
+              return (
+                <div key={episode.id}>
+                  <img src={episode.image.medium} alt={`${episode.name}`} />
+                  <div>{episode.name}</div>
+                  <div>
+                    Season: {episode.season} Episode: {episode.number}
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     </Fragment>
   )
--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 export const Store = React.createContext();
 const Provider = Store.Provider;
 
-const initialState = {};
+const initialState = {
+  episodes: [],
+  favourites: [],
+};
 
-const Reducer = () => {};
+const Reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+};
 
-export const StoreProvider = props => <Provider value='data from store'>{props.children}</Provider>;
+export const StoreProvider = props => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  const value = { state, dispatch };
+  return (
+    <Provider value={value}>{props.children}</Provider>
+  )
+} 
